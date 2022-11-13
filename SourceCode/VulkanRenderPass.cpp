e4a88921d8b83967a2eb32e8bbc0b5287ba876ef
scriptypie <miracl303@inbox.ru>
2022-11-13 15:48:19 +1000

    Update VulkanRenderPass.cpp

--- SourceCode/VulkanRenderPass.cpp
+++ SourceCode/VulkanRenderPass.cpp
@@ -21,6 +21,7 @@ CVulkanRenderPass::CVulkanRenderPass(PGraphicsContext &context, PSwapchain &swap
 
 CVulkanRenderPass::~CVulkanRenderPass() {
 	vkDestroyRenderPass(Accessors::GraphicsContext::GetDevice(m_Context), m_RenderPass, nullptr);
+	m_RenderPass = nullptr;
 }
 
 void CVulkanRenderPass::Begin(SCommandBuffer const &commandBuffer) {
@@ -91,18 +92,21 @@ void CVulkanRenderPass::CreateRenderPass() {
 
 	VkSubpassDependency dependency = {};
 	dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-	dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
-	dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
-	dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+	dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
 
 	VkAttachmentDescription attachments[] = {colorAttachment, depthAttachment};
+	VkSubpassDescription subpasses[] = {subpass};
+	VkSubpassDependency dependencies[] = {dependency};
+
 	VkRenderPassCreateInfo renderPassInfo = { VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO };
 	renderPassInfo.attachmentCount = FSize(attachments);
 	renderPassInfo.pAttachments = attachments;
-	renderPassInfo.subpassCount = 1;
-	renderPassInfo.pSubpasses = &subpass;
-	renderPassInfo.dependencyCount = 1;
-	renderPassInfo.pDependencies = &dependency;
+	renderPassInfo.subpassCount = FSize(subpasses);
+	renderPassInfo.pSubpasses = subpasses;
+	renderPassInfo.dependencyCount = FSize(dependencies);
+	renderPassInfo.pDependencies = dependencies;
 
 	if (vkCreateRenderPass(Accessors::GraphicsContext::GetDevice(m_Context), &renderPassInfo, nullptr, &m_RenderPass) != VK_SUCCESS)
 	ViAbort("Failed to create render pass!");
